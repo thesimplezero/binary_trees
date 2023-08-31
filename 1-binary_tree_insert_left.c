a9a8efc23ebc9949081ca9a396dddf828da5f596
thesimplezero <hi@thesimplezero.io>
2023-08-31 23:21:24 +0300

    test

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,35 +1,35 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - Inserts a left child into a binary tree
- * @parent: Pointer to the parent node of the node to create
- * @value: Value to store in the new left child
- * Return: Pointer to the new left child, or NULL on failure or if
- * parent is NULL
+ * binary_tree_insert_left - Inserts node as left-child of
+ *                           of another in binary tree.
+ * @parent: Pointer to node to insert the left-child in.
+ * @value: Value to store in the new node.
+ *
+ * Return: If parent is NULL or error occurs - NULL.
+ *         Otherwise - a pointer to new node.
+ *
+ * Description: If parent already has left-child, new node
+ *              takes its place and old left-child is set as
+ *              the left-child of new node.
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	/* Check if parent is NULL */
-	if (!parent)
+	binary_tree_t *new;
+
+	if (parent == NULL)
 		return (NULL);
 
-	binary_tree_t *left_child = malloc(sizeof(binary_tree_t));
-
-	if (!left_child)
+	new = binary_tree_node(parent, value);
+	if (new == NULL)
 		return (NULL);
 
-	left_child->parent = parent;
-	left_child->n = value;
-	left_child->left = parent->left;
-	left_child->right = NULL;
-
-	/* If parent already has a left child */
-	if (left_child->left)
+	if (parent->left != NULL)
 	{
-		left_child->left->parent = left_child;
+		new->left = parent->left;
+		parent->left->parent = new;
 	}
+	parent->left = new;
 
-	parent->left = left_child;
-
-	return (left_child);
+	return (new);
 }
