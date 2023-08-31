7e44ff382727bd53546c36faf0b71340d407b13c
thesimplezero <hi@thesimplezero.io>
2023-08-31 22:29:51 +0300

    Cleaned up the binary_tree_node function in 0-binary_tree_node.c

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,27 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Creates a binary tree node
+ * @parent: Pointer to the parent node of the node to create
+ * @value: Value to put in the new node
+ * Return: Pointer to the new node, or NULL on failure
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	/* Allocate memory for the new node */
+	new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+
+	/* Check if memory allocation was successful */
+	if (new_node == NULL)
+		return (NULL);
+
+	/* Initialize the new node's fields */
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
+}
